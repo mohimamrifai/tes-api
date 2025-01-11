892db5782124e8063e5274ca8202c72987144b3d
mohimamrifai <imamrifaibisnis@gmail.com>
2025-01-11 18:01:36 +0700

    set up and connect mongodb using mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express, { Application } from "express";
+import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
 const API_PREFIX = "/api/v1";
 
+const MONGO_URI = process.env.MONGO_URI || "";
 const PORT = process.env.PORT || 3000;
 
 app.use(`${API_PREFIX}/users`, userRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+mongoose.connect(MONGO_URI,).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log("Error connecting to MongoDB", err);
 });
