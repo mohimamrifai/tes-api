c24dc824923ffa36a674f0d2b8fc1cdc70071427
mohimamrifai <imamrifaibisnis@gmail.com>
2025-01-11 17:37:07 +0700

    create minimum express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express, { Application } from "express";
+import userRoutes from "./routes/userRoutes";
+
+const app: Application = express();
+const API_PREFIX = "/api/v1";
+
+const PORT = process.env.PORT || 3000;
+
+app.use(`${API_PREFIX}/users`, userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
