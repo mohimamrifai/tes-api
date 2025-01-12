a3ead3f0299cad32f16dd67371f6bd47c5599405
mohimamrifai <imamrifaibisnis@gmail.com>
2025-01-12 14:31:11 +0700

    create redirect to docs

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,10 @@ const logStream = fs.createWriteStream(path.join("/tmp", "requests.log"), { flag
 const swaggerFile = path.resolve(__dirname, "../docs/version.1.0.0.json");
 const swaggerData = JSON.parse(fs.readFileSync(swaggerFile, "utf-8"));
 
+// serve swagger files statically
+app.use(express.static(path.join(__dirname, "../docs")));
+app.use(express.static(path.join(__dirname, "../node_modules/swagger-ui-dist")));
+
 // middleware
 app.use(express.json());
 app.use(morgan("combined", { stream: logStream }));
