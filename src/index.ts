75d296807ffa4c9ae7f968e109bfcc9d4405ecb4
mohimamrifai <imamrifaibisnis@gmail.com>
2025-01-12 10:32:52 +0700

    add (logging) : create logging using morgan

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express, { Application } from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
 
@@ -10,10 +13,13 @@ const API_PREFIX = "/api/v1";
 
 const MONGO_URI = process.env.MONGO_URI || "";
 const PORT = process.env.PORT || 3000;
+const logStream = fs.createWriteStream(path.join(__dirname, "..", "requests.log"), { flags: "a" });
 
 // middleware
 app.use(express.json());
+app.use(morgan("combined", { stream: logStream }));
 
+// routes
 app.use(`${API_PREFIX}/users`, userRoutes);
 
 mongoose.connect(MONGO_URI,).then(() => {
